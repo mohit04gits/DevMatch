dc8cff7d02ccc49e8bae911926e13146d3cfa219
mohit04gits <mohit1834@gmail.com>
2025-08-03 11:30:06 +0530

    Fix: updated app logic and constants

--- frontend/src/utils/constants.jsx
+++ frontend/src/utils/constants.jsx
@@ -1,3 +1,3 @@
 // export  const BASE_URL = "http://localhost:3000"
 
-export  const BASE_URL = "https://devmatch-g1p3.onrender.com/"
+export  const BASE_URL = "https://devmatch-g1p3.onrender.com/" //hii
